63b993430b08794ee0cc30c271831ee2d750a0db
huxingyi1997 <huxingyi1997@sina.com>
2022-04-14 21:00:40 +0800

    featur/change_the_css

--- src/index.js
+++ src/index.js
@@ -6,40 +6,10 @@ import "./index.css";
 import { getPosition, calculateWinner } from './utils.js';
 
 // Square 组件渲染了一个单独的 <button>
-// class Square extends React.Component {
-//   // 添加一个构造函数，用来初始化 state
-//   // constructor(props) {
-//   //   // 每次你定义其子类的构造函数时，都需要调用 super 方法。因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。
-//   //   super(props);
-//   //   this.state = {
-//   //     value: null,
-//   //   };
-//   // }
-//   // 父组件引入后，Square组件不需要再保存游戏的 state
-
-//   render() {
-//     return (
-//       //   <button className="square" onClick={function() {alert('click'); }}>
-//       // 为了避免 this 造成的困扰，我们在这里使用箭头函数 来进行事件处理
-//       //   <button className="square" onClick={() => alert("click")}>
-//       // 次在组件中调用 setState 时，React 都会自动更新其子组件
-//       // <button className="square" onClick={() => this.setState({ value: "X" })}>
-//       // 接收父组件参数
-//       <button className="square" onClick={() => this.props.onClick()}>
-//         {/* 参数 */}
-//         {/* 原本是固定参数 */}
-//         {/* {this.props.value} */}
-//         {/* 改为变化参数 */}
-//         {/* {this.state.value} */}
-//         {/* 使用父组件参数 */}
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
-// 组件只包含一个 render 方法，并且不包含 state，那么使用函数组件简化
+// 组件只包含一个 render 方法，并且不包含 state，那么使用函数组件简化。父组件引入后，Square组件不需要再保存游戏的 state
 function Square(props) {
   return (
+    // 接收父组件参数
     <button className={props.className} onClick={props.onClick}>
       {props.value}
     </button>
@@ -48,46 +18,22 @@ function Square(props) {
 
 // Board 组件渲染了 9 个方块
 class Board extends React.Component {
-  // 将Board中的数据转移至Game父组件
-  // // 为 Board 组件添加构造函数，将 Board 组件的初始状态设置为长度为 9 的空值数组
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     squares: Array(9).fill(null),
-  //     // 将 “X” 默认设置为先手棋
-  //     xIsNext: true,
-  //   };
-  // }
-
-  // 移至Game组件
-  // 点击事件
-  // handleClick(i) {
-  //   const squares = this.state.squares.slice();
-  //   // 当有玩家胜出时，或者某个 Square 已经被填充时，该函数不做任何处理直接返回
-  //   if (calculateWinner(squares) || squares[i]) return;
-  //   // 根据xIsNext判断
-  //   squares[i] = this.state.xIsNext ? "X" : "O";
-  //   this.setState({
-  //     squares: squares,
-  //     // 翻转xIsNext
-  //     xIsNext: !this.state.xIsNext,
-  //   });
-  // }
-
+  // 将Board中的数据转移至Game父组件，无Constructor
+  // 渲染棋盘
   renderSquare(i) {
     // return <Square />;
-    const className = this.props.winner ?
-          this.props.winner.includes(i) ? 
-          'square win' : 'square' : 'square';
+    let className = this.props.winner ?
+      this.props.winner.includes(i) ?
+        'square win' : 'square' : 'square';
+    if (this.props.squares[i] === 'X') className += ' player1';
+    if (this.props.squares[i] === 'O') className += ' player2';
     // 增加参数，修改一下 Square 的点击事件监听函数
     return (
       <Square
-        // value={this.state.squares[i]}
-        // onClick={() => this.handleClick(i)}
         // 从 Game 组件中接收 squares 和 onClick 这两个 props。
         key={i % 3 + 1}
         value={this.props.squares[i]}
-        className = {className}
+        className={className}
         onClick={() => this.props.onClick(i)}
       />
     );
@@ -95,18 +41,6 @@ class Board extends React.Component {
 
   render() {
     // 移到Game组件
-    // // // 状态变化
-    // // const status = "Next player: " +  (this.state.xIsNext ? "X" : "O");
-    // // 计算胜者
-    // const winner = calculateWinner(this.state.squares);
-    // // 状态
-    // let status;
-    // // 判断是否获胜
-    // if (winner) {
-    //   status = '胜者：' + winner;
-    // } else {
-    //   status = "下一步: " +  (this.state.xIsNext ? "X" : "O");
-    // }
     // 渲染改为行列循环遍历
     const n = 3;
     const looper = Array(n).fill().map((_, i) => i + 1);
@@ -203,7 +137,7 @@ class Game extends React.Component {
   }
 
   render() {
-    // // 状态变化，从子组件提升到父组件
+    // 状态变化，从子组件提升到父组件
     // 历史
     const history = this.state.history;
     // 当前状态，将代码从始终根据最后一次移动渲染修改为根据当前 stepNumber 渲染
@@ -217,7 +151,7 @@ class Game extends React.Component {
     const moves = history.map((step, move) => {
       const display_moves = this.state.isStepsReverse ? history.length - move - 1 : move;
       const desc = display_moves ? "跳转至第" + display_moves + "步" : "游戏重新开始";
-      const className = move === step ? 'current-step' : '';
+      let className = move === step ? 'current-step' : '';
       const position = getPosition(step.position);
       return (
         // 组件的 key 值并不需要在全局都保证唯一，只需要在当前的同一级元素之前保证唯一即可
@@ -228,14 +162,25 @@ class Game extends React.Component {
     });
     // 状态
     let status;
+    let nextClass;
     // 判断是否获胜
     if (winner) {
       status = "胜者：" + winner;
+      nextClass = winner === "X" ? "player1" : "player2";
     } else if (isDraw) {
       status = "平局";
+      nextClass = "draw";
     } else {
       status = "下一步: " + (this.state.xIsNext ? "X" : "O");
+      nextClass = this.state.xIsNext ? "player1" : "player2";
     }
+    let players = [{
+      "className": "player1",
+      "name": "玩家1:X"
+    }, {
+      "className": "player2",
+      "name": "玩家2:O"
+    }];
     // 状态
     const isStepsReverse = this.state.isStepsReverse;
     let reverseButton = isStepsReverse ? "历史记录逆序" : "历史记录顺序";
@@ -243,6 +188,13 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
+          <div className="game-title">
+            {players.map((player) => {
+              return (<div className={player.className} key={player.className}>
+                {player.name}
+              </div>)
+            })}
+          </div>
           <Board
             // 绑定参数和事件
             winner={winnerIndex}
@@ -251,9 +203,9 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <button onClick={() => { this.reverseSteps() }}>{reverseButton}</button>
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <div className={nextClass + ' status'}>{status}</div>
+          <button className="reverse-steps" onClick={() => { this.reverseSteps() }}>{reverseButton}</button>
+          <ol className="history-list">{moves}</ol>
         </div>
       </div>
     );
